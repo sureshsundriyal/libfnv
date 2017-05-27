a8fb19b524381b3797a50c87b7757212725fa2c5
Suresh Sundriyal <sureshsundriyal@gmail.com>
2017-05-27 05:33:31 -0700

    move pyx to c

--- c/fnv1a.pyx
+++ c/fnv1a.pyx
@@ -3,3 +3,9 @@ cdef extern from "<stdint.h>" nogil:
 
 cdef extern from "c/fnv1a.h":
     void fnv1a(const char *foo, const int length, uint64_t *hash);
+
+
+def test():
+    cdef uint64_t x = <uint64_t>0
+    fnv1a("hello world", len("hello world") + 1, &x)
+    print x
