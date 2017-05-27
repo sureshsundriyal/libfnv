c3b3bfa541ffd254b146b0e63de90a597d3907c3
Suresh Sundriyal <sureshsundriyal@gmail.com>
2017-05-27 05:49:18 -0700

    Python module related Makefile changes

--- c/fnv1a.pyx
+++ c/fnv1a.pyx
@@ -1,7 +1,7 @@
 cdef extern from "<stdint.h>" nogil:
     ctypedef unsigned long long uint64_t
 
-cdef extern from "c/fnv1a.h":
+cdef extern from "fnv1a.h":
     void fnv1a(const char *foo, const int length, uint64_t *hash);
 
 
