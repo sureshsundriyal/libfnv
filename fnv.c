4e83602006b51701901a9ab0dd217f6b987dd1f7
Suresh Sundriyal <sureshsundriyal@gmail.com>
2017-05-28 03:04:55 -0700

    [fnv.c] Refactor some code.

--- fnv.c
+++ fnv.c
@@ -3,17 +3,18 @@
 
 #include "fnv.h"
 
-static const uint64_t fnv_prime = 0x100000001B3L;
+static const uint64_t fnv_prime_64  = 0x100000001B3L;
+static const uint64_t fnv_offset_64 = 0xcbf29ce484222325L;
 
 void fnv1_64(const char *msg, const int length, uint64_t *hash) {
 	int len = 0;
 
 	if (*hash == 0L) {
-		*hash = 0xcbf29ce484222325L;
+		*hash = fnv_offset_64;
 	}
 
 	while (len < length) {
-		*hash = ( (*hash * fnv_prime) ^ msg[len++] );
+		*hash = ( (*hash * fnv_prime_64) ^ msg[len++] );
 	}
 }
 
@@ -21,10 +22,10 @@ void fnv1a_64(const char *msg, const int length, uint64_t *hash) {
 	int len = 0;
 
 	if (*hash == 0L) {
-		*hash = 0xcbf29ce484222325L;
+		*hash = fnv_offset_64;
 	}
 
 	while (len < length) {
-		*hash = ( (*hash ^ msg[len++]) * fnv_prime );
+		*hash = ( (*hash ^ msg[len++]) * fnv_prime_64 );
 	}
 }
